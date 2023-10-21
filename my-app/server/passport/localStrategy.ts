f9cb3af022c6ef5eff16fe9fc6e1cb9547c61ca6
noah Kim <cpn1011@gmail.com>
2023-10-21 20:41:52 +0900

    update signin #1

--- my-app/server/passport/localStrategy.ts
+++ my-app/server/passport/localStrategy.ts
@@ -3,6 +3,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
 const memberDb = require("../models/member");
+const connection = require("../db/db");
 
 module.exports = () => {
   passport.use(
@@ -17,6 +18,36 @@ module.exports = () => {
         // console.log(inputId, inputPw);
 
         try {
+          await connection.query(
+            // id, pw 둘다 일치하는 데이터 찾아야함
+            // where에 2개의 조건 넣는 법
+            `SELECT member_id FROM member2 WHERE member_id = '${inputId}'`,
+            async function (err: Error, results: any[], fields: any) {
+              if (err) {
+                return done(err);
+              }
+              if (!results[0]) {
+                // 존재하지않는 로그인정보인 경우
+                return done(null, false, {
+                  message: "존재하지 않는 아이디입니다",
+                });
+              } else {
+                console.log(results);
+                const comparePassword = await bcrypt.compare(
+                  inputPw,
+                  results[0].member_pw
+                );
+
+                if (comparePassword) {
+                  return done(null, results[0]);
+                } else {
+                  return done(null, false, { message: "비번 틀렸음" });
+                }
+              }
+            }
+          );
+
+          // sequelize
           const findData = await memberDb.findOne({
             where: { member_id: inputId },
           });
